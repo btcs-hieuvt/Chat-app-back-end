db7bf3514c0ba72d29f294b484ed8a8f32ed3dfe
Vũ Trung Hiếu <hieuvt@btc-studio.io>
2023-01-29 19:16:06 +0700

    change url fe

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-front-end-seven.vercel.app",
     Credential: true,
   },
 });
